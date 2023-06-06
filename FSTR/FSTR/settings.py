fec7a702eb27962b883b531bbd5d4704697c4f35
Ilya Mikhasik <imikhassik@gmail.com>
2023-06-06 18:29:39 +0300

    Change settings for deploy

--- FSTR/FSTR/settings.py
+++ FSTR/FSTR/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['imikhassik.pythonanywhere.com',]
 
 
 # Application definition
@@ -121,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/imikhassik/FSTR_deploy/static'
+MEIDIA_ROOT = '/home/imikhassik/FSTR_deploy/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
